578f467905df5c0c8cf9c8fc58db95c914459575
Tom <twhitehead.1991@gmail.com>
2019-06-19 17:09:16 +0100

    Testing env variables

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import os
+
 env = 'prod'
+
+tester = os.environ.get('TESTER')
+print(tester)
 
 if env == 'dev':
     try:
