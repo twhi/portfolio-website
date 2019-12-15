83d5559f61a57fb70c30985583fa6b5985c9fef4
Tom <twhitehead.1991@gmail.com>
2019-12-15 13:54:35 +0000

    Added links to projects pages

--- jobs/models.py
+++ jobs/models.py
@@ -6,6 +6,8 @@ class Job(models.Model):
     title = models.CharField(max_length=256)
     description = models.TextField(default='')
     hover_text = models.CharField(max_length=256)
+    link_to_website = models.URLField(default='', max_length=2000, blank=True)
+    link_to_github = models.URLField(default='', max_length=2000, blank=True)
 
     def __str__(self):
         return self.title
