f178d95d8ee1eb1239a836def2c3999802a76754
Tom <twhitehead.1991@gmail.com>
2019-12-17 18:36:21 +0000

    changed blog cards

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('get_cv/', views.get_cv, name='get_cv')
 ]
--- home/views.py
+++ home/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import FileResponse, Http404
 
-def get_cv(request):
-    # try:
-    #     return FileResponse(open('static/home_static/CV.pdf', 'rb'), content_type='application/pdf')
-    # except FileNotFoundError:
-    #     raise Http404()
-    p = FileResponse(open('static/home_static/CV.pdf', 'rb'), content_type='application/pdf')
-    return p
-
 class Home(TemplateView):
     home_page = 'active'
     template_name = 'home.html'
